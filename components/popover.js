a0115f7fd131f74e4b2bebb495391cf6d1066add
Lionel ZUBER <lionel.zuber@armaklan.org>
2022-02-13 12:23:52 +0100

    Amelioration design popover

--- components/popover.js
+++ components/popover.js
@@ -16,20 +16,33 @@ export class Manoeuvre extends LitElement {
       opacity: 0;
       visibility: hidden;
       position: absolute;
-      background-color: #F3F3F3;
+      background-color: #f3f3f3;
       box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 1px,
         rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px,
         rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px;
       border-radius: 0.25rem;
       padding: 0.2rem 0.6rem;
       height: fit-content;
-      top: 110%;
+      top: -120%;
       font-size: 0.8rem;
       width: 86%;
       left: 4%;
-      transition: all 0.5s cubic-bezier(0.75, -0.02, 0.2, 0.97);
+      transition: all 0.3s cubic-bezier(0.75, -0.02, 0.2, 0.97);
       z-index: 10;
     }
+
+    .popover:after {
+      position: absolute;
+      z-index: -1;
+      content: "";
+      right: calc(50% - 10px);
+      top: 100%;
+      border-style: solid;
+      border-width: 10px 10px 0px 10px;
+      border-color: #f3f3f3 transparent transparent transparent;
+      transition: all 0.3s cubic-bezier(0.75, -0.02, 0.2, 0.97);
+    }
+
     :host {
       display: block;
       position: relative;
