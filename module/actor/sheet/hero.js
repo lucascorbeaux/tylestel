8104cb9796c938b1fce69db07b9d572cc77ec633
Lionel ZUBER <lionel.zuber@armaklan.org>
2022-02-13 16:19:58 +0100

    Fix nbManoeuvres

--- module/actor/sheet/hero.js
+++ module/actor/sheet/hero.js
@@ -43,8 +43,8 @@ export default class HeroSheet extends ActorSheet {
     sheetData.equipements = encodeToLitObject(getItems(sheetData, "equipement"));
     sheetData.familiers = encodeToLitObject(getItems(sheetData, "familier"));
 
-    sheetData.nbActions = sheetData.data.data.metiers.guerrier / 2 + 2;
-    sheetData.nbReactions = sheetData.data.data.metiers.stratégos / 2 + 1;
+    sheetData.nbActions = Math.floor(sheetData.data.data.metiers.guerrier / 2) + 2;
+    sheetData.nbReactions = Math.floor(sheetData.data.data.metiers.stratégos / 2) + 1;
     
     console.debug(sheetData);
     return sheetData;
