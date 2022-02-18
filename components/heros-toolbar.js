6c9ce8e6f8e4889c67f25beb07fd054d41830c37
Armaklan <lionel.zuber@armaklan.org>
2022-02-18 23:34:23 +0100

    Ajout compendium macro

--- components/heros-toolbar.js
+++ components/heros-toolbar.js
@@ -1,6 +1,6 @@
 import { LitElement, css, html } from "https://unpkg.com/lit?module";
 import { buttonCss } from "./cssCommun.js";
-import { launchDice } from "../services/dice.js";
+import { Macro } from "../module/macro.js";
 
 export class HerosToolbar extends LitElement {
   static properties = {
@@ -67,80 +67,16 @@ export class HerosToolbar extends LitElement {
   }
 
   async encaisserDommage() {
-    openDialog(
-      "Encaisser des dommages",
-      "number-dialog",
-      this.actor,
-      (data) => {
-        const nbDommage = data.numField.value;
-        if (!nbDommage || nbDommage <= 0) {
-          ui.notifications.error(`Le nombre de dommage doit être positif`);
-        }
-        const actor = this.actor;
-        const currentVieRessource = actor.data.data.vie;
-
-        updateActorData(actor, "vie", {
-          ...currentVieRessource,
-          current: currentVieRessource.current - Number(nbDommage),
-        });
-      }
-    );
+    Macro.takeDamage(this.actor);
   }
 
   async regagnerVie() {
-    openDialog("Regagner de la vie", "number-dialog", this.actor, (data) => {
-      const nbVie = data.numField.value;
-      if (!nbVie || nbVie <= 0) {
-        ui.notifications.error(`Le nombre de pts regagné doit être positif`);
-      }
-      const actor = this.actor;
-      const currentVieRessource = actor.data.data.vie;
-
-      updateActorData(actor, "vie", {
-        ...currentVieRessource,
-        current: currentVieRessource.current + Number(nbVie),
-      });
-    });
+    Macro.regainLife(this.actor);
   }
 
   async effectuerTest() {
-    openDialog("Effectuer une action", "choix-test", this.actor, (data) => {
-      const attribut = data.attribut.value;
-      const metier = data.metier.value;
-      launchDice(this.actorId, attribut, metier);
-    });
+    Macro.makeTest(this.actor);
   }
-}
-
-
-export function updateActorData(actor, key, updatedProperties) {
-    const data = { data: {} };
-    data.data[key] = updatedProperties;
-    actor.update(data);
-}
-
-export async function openDialog(title, template, actor, callback) {
-    const urlTemplate = `systems/tylestel/templates/dialog/${template}.html`;
-    const content = await renderTemplate(urlTemplate, {actor});
-
-    return new Promise((resolve) => {
-        new Dialog({
-          title,
-          content,
-          buttons: {
-            std: {
-              label: "Valider",
-              callback: (html) => {
-                  const dialogData = html[0].querySelector('form');
-                  callback(dialogData);
-              },
-            },
-          },
-          close: (html) => {
-            resolve();
-          },
-        }).render(true);
-    });
 }
 
 export function declareHerosToolbar() {
