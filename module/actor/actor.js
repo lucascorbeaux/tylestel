ef95aab947b7d7353e5a8768415cb42495d8c798
Armaklan <lionel.zuber@armaklan.org>
2022-02-20 13:04:03 +0100

    Prise en compte armure et modificateur d'initiative

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -1,3 +1,4 @@
+import { getItems } from "../helper.js";
 export default class ActorTylestel extends Actor {
   /** @override */
   prepareData() {
@@ -11,13 +12,20 @@ export default class ActorTylestel extends Actor {
       data.malusEtat = Object.values(data.etats)
         .filter((e) => e.actif)
         .reduce((prev, curr) => prev + curr.malus, 0);
+      data.valeurArmure = this.calculValeurArmure();
+      data.modificateurInitiative = this.calculModificateurInitiative();
       const perception = data.attributs.perception;
       const agilite = data.attributs.agilité;
-      data.initRoll = `1d6x+1d6+${perception}+${agilite}-${data.malusEtat}`;
+      data.initRoll = `1d6x+1d6+${perception}+${agilite}-${data.malusEtat}+${data.modificateurInitiative}`;
     }
 
     if (actorData.type === "monstres") {
-      data.vie.current = this.validateMinMaxData(data.vie.current, data.vie.min, data.vie.max);
+      data.vie.current = this.validateMinMaxData(
+        data.vie.current,
+        data.vie.min,
+        data.vie.max
+      );
+      data.valeurArmure = data.resistance;
       const corps = data.initiative;
       data.initRoll = `${corps}`;
     }
@@ -49,4 +57,22 @@ export default class ActorTylestel extends Actor {
     };
   }
 
+  calculValeurArmure() {
+    return getItems(this.data, "armure")
+      .filter((a) => a.data.data.equipe)
+      .map((a) => a.data.data.resistance)
+      .reduce((prev, curr) => Math.max(prev, curr), 0);
+  }
+
+  calculModificateurInitiative() {
+    return getItems(this.data, "arme")
+      .filter((a) => a.data.data.equipe)
+      .map((a) => a.data.data.initiative)
+      .reduce((prev, curr) => {
+        if(!prev) {
+          return curr;
+        }
+        return Math.min(prev, curr);
+      }, null) || 0;
+  }
 }
